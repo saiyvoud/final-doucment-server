c6b74dc90a59d0e31193d0e2a32575d90bd94aba
beesaiyvoud <saiyvouds@gmail.com>
2025-07-20 21:24:17 +0700

    add

--- src/config/db_mysql.js
+++ src/config/db_mysql.js
@@ -1,9 +1,21 @@
 import mysql from "mysql";
-const connected = mysql.createConnection({
-    // host: "localhost",
-    // user: "root",
-    // password: "",
-    // database: "db_document",
+// const connected = mysql.createConnection({
+//     // host: "localhost",
+//     // user: "root",
+//     // password: "",
+//     // database: "db_document",
+//     waitForConnections: true,
+//     connectionLimit: 10,
+//     queueLimit: 0,
+//     host: "mysql-199842-0.cloudclusters.net",
+//     port: "16517",
+//     user: "admin",
+//     password: "mGkW3Tmk",
+//     database: "db_document"
+// });
+
+
+const connected = mysql.createPool({
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
@@ -14,8 +26,14 @@ const connected = mysql.createConnection({
     database: "db_document"
 });
 
-connected.connect((err) => {
-    if (err) console.log(`Faild Connect Database`, err);
-    console.log(`Connected Database`);
-})
+// Test connection with a simple query
+connected.getConnection((err, connection) => {
+    if (err) {
+        console.error("Failed to connect to database:", err);
+    } else {
+        console.log("Connected to database");
+        connection.release(); // release back to pool
+    }
+});
+
 export default connected;
